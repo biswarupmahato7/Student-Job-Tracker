96d953b1466760cddc2f721d32b491f1cb1e2a4b
Biswarup Mahato <107302651+biswarupmahato7@users.noreply.github.com>
2025-04-10 13:13:00 +0530

    some minore issu fixe

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI , {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err));
+console.log(process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 app.use('/api/job-applications', jobApplicationsRoute);
 
